11599ade0fe638035528dee85e7d10cdeff8ac8a
沼田悠貴 <chachamalun@gmail.com>
2018-08-14 15:57:14 +0900

    以前実現していたものの再現(lti)

--- lti/app/controllers/application_controller.rb
+++ lti/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  #CSRF対策を無効にする
+  protect_from_forgery :except => ["create"]
 end
--- lti/app/controllers/home_controller.rb
+++ lti/app/controllers/home_controller.rb
@@ -7,6 +7,11 @@ class HomeController < ApplicationController
   end
 
   def create
+    #x-frameでの表示をすべてに許可する
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
+
+    #postで送られてきた値のシンボルを指定し@tempに代入
+    @temp = params[:lti_message_type]
   end
 
 end
