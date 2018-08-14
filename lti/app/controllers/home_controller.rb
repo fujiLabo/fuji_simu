83b5147168b87f7e231371a9b1b38b6aa7ecb5db
沼田悠貴 <chachamalun@gmail.com>
2018-08-14 10:57:50 +0900

    最低限のルート作成(lti)

---
+++ lti/app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def top
+
+  end
+
+  def temp
+  end
+
+  def create
+  end
+
+end
--- lti/config/routes.rb
+++ lti/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #getページ
+  get "/home/top" => "home#top"
+  get "/home/temp" => "home#temp"
+
+  #lti環境練習の際に接続するpostページ
+  post "/home/create" => "home#create"
 end
