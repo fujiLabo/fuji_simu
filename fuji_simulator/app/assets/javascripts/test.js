10a23632d616ae229fca35d6f71c01d0d0103228
沼田悠貴 <chachamalun@gmail.com>
2018-08-18 04:00:04 +0900

    pcの画像を2つ用意し、ダブルクリックされると画像が消える

--- fuji_simulator/app/assets/javascripts/test.js
+++ fuji_simulator/app/assets/javascripts/test.js
@@ -2,6 +2,12 @@
 //この書き方だとリロードでもjqueryを読み込む
 $(document).on('turbolinks:load', function() {
   $('p').text("JQuery可動テスト(稼働中)");
+
+
+  $('.PC').dblclick(function(){
+    $(this).fadeOut();
+
+  })
 });
 
 $(function(){
