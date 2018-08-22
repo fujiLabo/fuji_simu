1498ce1f2604e6eba2f7ae5281ac09f22d441576
沼田悠貴 <chachamalun@gmail.com>
2018-08-22 13:01:20 +0900

    自作したボタンの表示

--- fuji_simulator/app/assets/javascripts/test.js
+++ fuji_simulator/app/assets/javascripts/test.js
@@ -1,7 +1,7 @@
 
 //この書き方だとリロードでもjqueryを読み込む
 $(document).on('turbolinks:load', function() {
-  $('p').text("JQuery可動テスト(稼働中)");
+  $('.page p').text("JQuery可動テスト(稼働中)");
 
 
   $('.PC').bind('contextmenu' , function(){
