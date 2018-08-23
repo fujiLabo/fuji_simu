7ec5b41b6cc3d8571df13764976ad5606a5bd58f
沼田悠貴 <chachamalun@gmail.com>
2018-08-23 16:42:34 +0900

    lti2の追加。oauthの導入(oauth_providerやoauthのgemを試す用) ltiがおかしくなったので最低限動くように修正

--- lti/config/routes.rb
+++ lti/config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  #getページ
   get "/home/top" => "home#top"
-  get "/home/temp" => "home#temp"
-
-  #lti環境練習の際に接続するpostページ
   post "/home/create" => "home#create"
+  get "/home/temp" => "home#temp"
 end
