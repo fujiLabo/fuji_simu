5610e11ae28e48d7ae059aea212fd39e96e1ffdb
沼田悠貴 <chachamalun@gmail.com>
2018-09-27 16:35:53 +0900

    lti2の削除&fuji_simulator2の作成

--- fuji_simulator/app/assets/javascripts/test.js
+++ fuji_simulator/app/assets/javascripts/test.js
@@ -3,9 +3,10 @@ function createContextMenu()
   $.contextMenu( 'destroy' );
   $.contextMenu({
     selector:"#contextMenuArea"
-    ,items: $.contextMenu.fromMenu($("#contextMenuTemplate"))
+    items: $.contextMenu.fromMenu($("#contextMenuTemplate"))
   });
 }
+
 
 
 
@@ -27,19 +28,6 @@ $('.PC').animateClick();
     $(this).fadeOut();
     return false;
   })
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-  $("#div1").draggable();
-
-=======
-  
->>>>>>> f62839404bbc30684479f33ea542ba2d562b4066
-});
-
-=======
->>>>>>> 1fb7c9d1132e1742423da98b5acd5f3eb20dde5a
-
   //$("#div1").draggable();
 
 });
