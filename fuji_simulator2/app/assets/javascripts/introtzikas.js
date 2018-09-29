4efeb77d94607f0dad4973efc6df4f9b673a4d82
沼田悠貴 <chachamalun@gmail.com>
2018-09-29 21:42:52 +0900

    classの大枠の設定

--- fuji_simulator2/app/assets/javascripts/introtzikas.js
+++ fuji_simulator2/app/assets/javascripts/introtzikas.js
@@ -79,4 +79,4 @@ THE SOFTWARE.
 
 		  });
   };
-})( jQuery );
+});
--- fuji_simulator2/app/assets/javascripts/test.js
+++ fuji_simulator2/app/assets/javascripts/test.js
@@ -5,16 +5,17 @@ $(document).on('turbolinks:load', function(){
   $('.PC').draggable();
 
 
-
+  /*
   //読み込み時のアニメーション ()
-  $("#intro").introtzikas({
+  $().introtzikas({
     line: '#fff', //ラインの色
     speedwidth: 1000, //幅の移動完了スピード
     speedheight: 1000, //高さの移動完了スピード
     bg: '#333' //背景色
   });
+  */
 
-  
+
 
 
 });
