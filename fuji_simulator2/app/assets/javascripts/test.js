b8731ca832bdff7ebe4c47c02dcff59a214885a1
沼田悠貴 <chachamalun@gmail.com>
2018-10-04 17:28:22 +0900

    PCやルータをmainにドロップすると別の処理を行う

--- fuji_simulator2/app/assets/javascripts/test.js
+++ fuji_simulator2/app/assets/javascripts/test.js
@@ -1,3 +1,6 @@
+
+mainDropFlg = true;
+
 //この書き方ならリロード時もjqueryを読み込む
 $(document).on('turbolinks:load', function(){
   $('#text').text('jquery実装');
@@ -5,6 +8,30 @@ $(document).on('turbolinks:load', function(){
   $('.PC').draggable();
 
 
+  //PCやルータのドラッグ設定
+  $('.machine').draggable({
+    helper: 'clone',
+    revert: true,
+    zIndex: 3,
+
+    start: function(e, ui){$(this).addClass('dragout')},
+    stop: function(e, ui){$(this).removeClass('dragout')},
+  });
+
+  $('#ns_main').droppable({
+    accept: '.machine',
+    tolerance: 'fit',
+
+    drop: function(e, ui){
+      mainDropFlg = false;
+    },
+    deactivate: function(e, ui){
+      ui.draggable.draggable({ revert: mainDropFlg});
+      if (mainDropFlg == false){
+        mainDropFlg = true;
+      }
+    }
+  });
   /*
   //読み込み時のアニメーション ()
   $().introtzikas({
