1865861db876c15cf637d2c32ff662bb17928738
菅原良太 <>
2018-10-15 02:07:19 +0900

    暗号化一歩手前までやった　base64エンコード hmac-sha1がうまくいってない

--- lti/app/controllers/home_controller.rb
+++ lti/app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ require 'oauth'
 require 'uri'
 require "addressable/uri"
 require 'cgi'
+require 'digest/hmac'
+
 class HomeController < ApplicationController
 
   def top
@@ -12,6 +14,10 @@ class HomeController < ApplicationController
   end
 
   def create
+    @METHOD = "POST"
+    @KEY = "2e45c0e27fb80e8fbf3d8ad3000d91bf&"
+    @REQUEST = CGI.escape("http://localhost:3000/home/create")
+
     #x-frameでの表示をすべてに許可する
     response.headers['X-Frame-Options'] = 'ALLOWALL'
 
@@ -24,31 +30,53 @@ class HomeController < ApplicationController
     @temp.delete(:oauth_signature)
     #ハッシュかする
     @temp = @temp.permit!.to_hash
+
+    @temp.each { |key, value|
+      @tempString = CGI.escape(value)
+      @tempString.gsub!('+','%20')
+      @temp[key] = @tempString
+    }
     #ソートする このとき配列になる
     @sort = @temp.sort
+
+=begin
+    @sort.each  { |keyvalue|
+      @string += keyvalue
+    }
+    @string.gsub!(' ','+')
 
     #ソート済みハッシュへ
     @sortHash = Hash[*@sort.flatten]
 
     puts URI.encode_www_form(@sortHash)
+=end
 
+ #自力でやる方
+    @string = @sort[0][0] + "=" + @sort[0][1]
 
-  #@string = @sort[0][0] + "=" + @sort[0][1]
+    @sort[1..-1].each{ |keyvalue|
+      @string += "&" + keyvalue[0] + "=" + keyvalue[1]
+    }
 
-    #@sort[1..-1].each{ |keyvalue|
-    #  @string += "&" + keyvalue[0] + "=" + keyvalue[1]
-    #}
-    #@string.gsub!(' ','+')
+=begin
+    @string.gsub!(' ','+')
+    @string.gsub!('+','%20')
+    @string.gsub!('&','%26')
+    @string.gsub!(':','%3A')
+    @string.gsub!('/','%2F')
+    @string.gsub!('=','%3D')
+=end
 
-
-
-
-    #URI.encode_www_form()
     #puts @string
+    @Signature_base_string = @METHOD
+    @Signature_base_string += "&" + @REQUEST
+    @Signature_base_string += "&" + CGI.escape(@string)
+
+    puts @Signature_base_string
+    #puts Base64.encode64(OpenSSL::HMAC::digest(OpenSSL::Digest::SHA1.new,@KEY,@Signature_base_string))
     #@new = Addressable::URI.parse(@string)
     #@new = URI.parse(@string)
     #puts @new
-
 
 
 
