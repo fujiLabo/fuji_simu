45b890d2cfb4fdc6e10af15c6345fd9889a9c8a8
沼田悠貴 <chachamalun@gmail.com>
2018-10-15 18:48:18 +0900

    ひとまず右クリック時別処理を実行化(変更？)

--- fuji_simulator2/app/assets/javascripts/test.js
+++ fuji_simulator2/app/assets/javascripts/test.js
@@ -7,13 +7,19 @@ $(document).on('turbolinks:load', function(){
 
   function fncontextmenu(element)
   {
-    $.contextMenu( 'destroy' );
-    return false;
+    console.log("test");
 
     document.oncontextmenu = function () {
        return false;
     };
+
+    $.contextMenu( 'destroy' );
   }
+
+  $(document).on('contextmenu', '.dropMachine', function(){
+    fncontextmenu(this);
+
+  });
 
 
   //PCやルータのドラッグ設定
@@ -58,20 +64,21 @@ fnMainDrop = function(ui, obj)
   $('#ns_main').append(
     $('<img>').attr({
       src: ui.draggable.attr('src'),
+      class: 'dropMachine',
+      id: "test",
       style: "position: absolute; top: "+ ui.offset.top + "px; left: "+ ui.offset.left +"px",
 
     })
 
   );
-  $("#ns_main img:last-child").attr('oncontextmenu', 'return fncontextmenu(this)');
+  //先代はこのコード属性を与え、関数を呼ぼ出している
+  //$("#ns_main img:last-child").attr('oncontextmenu', 'return fncontextmenu(this)');
+
   //mainにドロップされたものをドラッグ可能に(オプションによってmain内でのみに移動を限定する必要あり)
   $('#ns_main img:last-child').draggable({
   });
 }
 
-document.oncontextmenu = function () {
-   return false;
-};
 
 
 });
