8b9ef6a06f23172eb470cf926431bde67b3079f2
菅原良太 <>
2018-10-26 17:26:28 +0900

    リダイレクト関係終わり　パラメータの使い方について学習した

--- lti/config/routes.rb
+++ lti/config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   post "/home/create" => "home#create"
   get "/home/temp" => "home#temp"
   get "/home/tstamp" => "home#tstamp"
+  get "/home/error" => "home#error"
 end
