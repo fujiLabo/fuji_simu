cea7b9e0cfe02f8d4d24029361d64e7e72dcc990
沼田悠貴 <chachamalun@gmail.com>
2018-10-26 18:08:33 +0900

    線の描画テスト

--- fuji_simulator2/app/assets/javascripts/simuDec.js
+++ fuji_simulator2/app/assets/javascripts/simuDec.js
@@ -1,31 +1,53 @@
 //変数を宣言するファイル
-$(function(){
+$(function() {
 
-//変数の定義
-//空のオブジェクトを作成
-if ( typeof NS === "undefined" ) NS = {};
+  //変数の定義
+  //空のオブジェクトを作成
+  if (typeof NS === "undefined") NS = {};
 
-NS.pcNode = 0;
+  //全体
+  NS.pcNode = 0;
+  NS.ruNode = 0;
+  NS.busNode = 0;
+  NS.swNode = 0;
+  NS.svNode = 0;
+  NS.lanNode = 0;
 
-NS.mainDropFlg = true; //draggableのフラグ
-
-//canvasに関する
-NS.points       = []; //ドラッグ時のマウスの座標の配列
-NS.canvasWidth  = $('#ns_main').width(); //ns_mainの幅
-NS.canvasHeight = $('#ns_main').height(); //ns_mainの高さ
-console.log("canvasHeight: " + NS.canvasHeight);
-
-var canvas = document.getElementById('ns_main_canvas');
-
-
-//var test = $('#ns_main_canvas')[0].getBoundingClientRect().left - 35;
-NS.mainCtx      = $('#ns_main_canvas')[0].getContext('2d'); //ns_main_canvasをAPIにアクセスできるオブジェクト
-
-$('#ns_main_canvas').attr('data-x', NS.mainCanvasWidth);
-$('#ns-main-canvas').attr('data-y', NS.mainCanvasHeight);
+  //main部分の変数
+  NS.mainDropFlg = true; //draggableのフラグ
+  NS.dropNodeInt = 0;
+  NS.dropNodeName = "";
+  NS.dropContextName = "";
 
 
+  //canvasに関する
+  NS.points           = []; //ドラッグ時のマウスの座標の配列
+  NS.lanArrClass      = []; //ns_main_canvasのクラスの配列
+  NS.lanWidth         = 2;  //LANの幅
+  NS.canvasWidth      = $('#ns_main').width(); //ns_mainの幅
+  NS.canvasHeight     = $('#ns_main').height(); //ns_mainの高さ
+  console.log("canvasHeight: " + NS.canvasHeight);
 
-NS.lanFlag = false; //LAN画像のonとoffに関するフラグ
+  NS.mainCanvasWidth  = $('#ns_main_canvas')[0].getBoundingClientRect().left;
+  console.log("canvas確認: " + NS.mainCanvasWidth);
+  NS.mainCanvasHeight = $('#ns_main_canvas')[0].getBoundingClientRect().top;
+
+  NS.elLanMoveThis;
+  NS.addCtx;  //追加したCanvasのAPIにアクセスできるオブジェクト
+  NS.addCanvas; //追加したCanvasを格納
+
+  NS.mainCtx = $('#ns_main_canvas')[0].getContext('2d'); //ns_main_canvasをAPIにアクセスできるオブジェクト
+  NS.mainCtx.strokeStyle = 'red';
+
+  $('#ns_main_canvas').attr('data-x', NS.mainCanvasWidth);
+  $('#ns-main-canvas').attr('data-y', NS.mainCanvasHeight);
+
+
+
+  NS.lanFlag = false; //LAN画像のonとoffに関するフラグ
+  NS.lanLinkFlag; //
+  NS.lanPointFlag;
+  NS.lanDeleteFlag;
+  NS.lanMoveFlag;
 
 });
--- fuji_simulator2/app/assets/javascripts/simuMain.js
+++ fuji_simulator2/app/assets/javascripts/simuMain.js
@@ -7,10 +7,10 @@ $.when(
   $.getScript("/assets/simuDec.js"),
   $.getScript("/assets/simuFunc.js"),
 ).then(function(){
+
+
   console.log("test");
-  //console.log(don);
   console.log("NS.canvasHeight(main): " + NS.canvasHeight)
-  //NS.mainCtx      = $('#ns_main_canvas')[0].getContext('2d');
   console.log("test: " + NS.mainCtx);
   //PCやルータのドラッグ設定
   $('.machine').draggable({
@@ -48,6 +48,7 @@ $.when(
     changeLanMode();
     console.log("LAN押された");
   });
+
 
 
 });
