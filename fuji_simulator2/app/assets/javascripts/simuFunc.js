3103317b460269616ebd5958d62f6018c9172641
沼田悠貴 <chachamalun@gmail.com>
2018-11-03 01:11:49 +0900

    PC用右クリック完成(静的)

--- fuji_simulator2/app/assets/javascripts/simuFunc.js
+++ fuji_simulator2/app/assets/javascripts/simuFunc.js
@@ -46,7 +46,7 @@
 
     $.contextMenu({
       selector: ".dropMachine",
-      items: $.contextMenu.fromMenu($('#contexttest'))
+      items: $.contextMenu.fromMenu($('#contextPC'))
     });
   }
   //この書き方でも一応右クリックできた(ダブルクリックにするならこれ？)
