ae93035f4658947ad29a8580a87f05c87cacf834
沼田悠貴 <chachamalun@gmail.com>
2018-11-07 23:54:25 +0900

    無駄改善(関数化はしなくてもいいかも)

--- fuji_simulator2/app/assets/javascripts/simuFunc.js
+++ fuji_simulator2/app/assets/javascripts/simuFunc.js
@@ -64,9 +64,14 @@
     $("#ns_main img:last-child").attr('oncontextmenu', 'return fncontextmenu(this)');
 
     //mainにドロップされたものをドラッグ可能に(オプションによってmain内でのみに移動を限定する必要あり)
-    $('#ns_main img:last-child').draggable({});
+    if (NS.lanFlag){
+      $('#ns_main img:last-child').mouseup(function(e) {e.preventDefault(e); });
+      $('#ns_main img:last-child').mousedown(function(e) {e.preventDefault(e); });
+    }else{
+      $('#ns_main img:last-child').draggable({});
+    }
 
-    changeDrag();
+    //changeDrag();
   }
 
   //線の描画
