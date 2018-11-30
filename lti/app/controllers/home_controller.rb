19bbd2c8a705b52d7280141d8815c57cf6b07cdd
菅原良太 <>
2018-11-30 19:21:38 +0900

    OAuth_hash作ったり色々した

--- lti/app/controllers/home_controller.rb
+++ lti/app/controllers/home_controller.rb
@@ -24,6 +24,13 @@ class HomeController < ApplicationController
   end
 
   def create
+    #XMLの読み込み
+    f = File.open("aa.xml", "r")
+    @File_read_Xml = f.read
+
+    #Oauth_hash作成
+    @Body_hash = CGI.escape(Base64.encode64(Digest::SHA1.digest(@File_read_Xml)).chomp)
+
     #timestampを取得して格納
     @timestamp = Time.now.to_i.to_s
     @METHOD = "POST"
@@ -42,6 +49,9 @@ class HomeController < ApplicationController
     #postで送られてきた値のシンボルを指定し@tempに代入
     #@temp = params.permit!.to_hash
     @temp = params
+
+    #Body_hashの追加
+    @temp[:oauth_body_hash] = @Body_hash
     #戻り値が文字列だった
     @temp.delete(:action)
     @temp.delete(:controller)
@@ -127,32 +137,28 @@ class HomeController < ApplicationController
 =end
 
 
-  @uri = URI.parse(@Return_grade)
+  $uri = URI.parse(@Return_grade)
 
-  @uri_2 = URI.parse(@Return_url)
-
-
-  @File_read_Xml = File.open("aa.xml", "r")
+  $uri_2 = URI.parse(@Return_url)
 
 
-#OAuth_hashを作るぞ〜〜〜〜
-
-puts @Oauth_strings = "OAuth realm" + "=" + "\"http://sp.example.com/\"" + "," +
+puts $Oauth_strings = "OAuth realm" + "=" + "\"\"" + "," +
                  "oauth_consumer_key" + "=" + "\"" + @Oauth_Consumer_key + "\"" + "," +
                  "oauth_signature_method" + "=" + "\"HMAC-SHA1\"" + "," +
                  "oauth_timestamp" + "=" + "\"" + @timestamp + "\"" + "," +
                  "oauth_nonce" + "=" + "\"" + @Nonce + "\"" + "," +
                  "oauth_version" + "=" + "\"" + "1.0" + "\"" "," +
-                 "oauth_signature" + "=" + "\"" + @oauth_signature + "\""
+                 "oauth_signature" + "=" + "\"" + @oauth_signature + "\"" + "," +
+                 "oauth_body_hash" + "=" + "\"" + @Body_hash + "\""
 
 #=begin
                    header = {'Content-Type': 'application/xml',
-                     'Authorization': @Oauth_strings
+                     'Authorization': $Oauth_strings
                    }
 #=end
 
-  doc = REXML::Document.new(@File_read_Xml)
-  element = doc.elements['imsx_POXEnvelopeRequest/imsx_POXBody/replaceResultRequest/resultRecord/sourcedGUID/sourcedId']
+  $doc = REXML::Document.new(@File_read_Xml)
+  element = $doc.elements['imsx_POXEnvelopeRequest/imsx_POXBody/replaceResultRequest/resultRecord/sourcedGUID/sourcedId']
   element.text = nil
   element.add_text(@SourcedId)
   #puts doc.to_s
@@ -160,7 +166,7 @@ puts @Oauth_strings = "OAuth realm" + "=" + "\"http://sp.example.com/\"" + "," +
 
 #=end
 
-#=begin
+=begin
   #create the http object
   http = Net::HTTP.new(@uri.host, @uri.port)
   http.use_ssl = false
@@ -168,7 +174,7 @@ puts @Oauth_strings = "OAuth realm" + "=" + "\"http://sp.example.com/\"" + "," +
   request["Content-Type"] = "application/xml"
   request["Authorization"] = @Oauth_strings
   request.body = doc.to_s
-#=end
+=end
 
 
 =begin
@@ -183,10 +189,34 @@ request.body = doc.to_s
 =end
 
   #send the request
-  response = http.request(request)
+  #response = http.request(request)
   #puts @Oauth_strings
 
 
   end
+  def grade
+#=begin
+    http = Net::HTTP.new($uri.host, $uri.port)
+    http.use_ssl = false
+    request = Net::HTTP::Post.new($uri.request_uri)
+    request["Content-Type"] = "application/xml"
+    request["Authorization"] = $Oauth_strings
+    request.body = $doc.to_s
 
+    response = http.request(request)
+#=end
+
+=begin
+    http = Net::HTTP.new("133.14.14.232", 80)
+    http.use_ssl = false
+    request = Net::HTTP::Post.new("http://133.14.14.232")
+    request["Content-Type"] = "application/xml"
+    request["Authorization"] = $Oauth_strings
+    request.body = $doc.to_s
+
+    response = http.request(request)
+=end
+  end
+
+helper_method :grade
 end
