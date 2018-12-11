e8c5d1e67743e37740035da0e4503a82387b22d8
沼田悠貴 <chachamalun@gmail.com>
2018-12-11 17:54:12 +0900

    素材本来! css先代準拠

--- fuji_simulator3/app/assets/javascripts/simuMain.js
+++ fuji_simulator3/app/assets/javascripts/simuMain.js
@@ -1,13 +1,12 @@
 //mainファイル
-//直接htmlを操作する
 
 //他のファイルが読み込まれてからこのファイルを読み込む
-// $.when(
-//   $.ready,
-//   $.getScript("/assets/simuDec.js"),
-//   $.getScript("/assets/simuFunc.js"),
-// ).then(function(){
-$(function(){
+$.when(
+  $.ready,
+  $.getScript("/assets/simuDec.js"),
+  $.getScript("/assets/simuFunc.js"),
+).then(function(){
+
 
   /*
    * ネットワークシミュレータのプログラム
@@ -726,68 +725,6 @@ $(function(){
     //初期位置に戻す
     $('html').scrollTop(0);
     $('html').scrollLeft(0);
-
-  // 変数の定義
-
-  if ( typeof NSF === "undefined" ) var NSF = {};
-
-  // nsf-all var
-  NSF.pcNode = 0;  // PCの数
-  NSF.ruNode = 0;  // Routerの数
-  NSF.busNode = 0; //busの数
-  NSF.swNode = 0;  // Switchの数
-  NSF.svNode = 0;  // Serverの数
-  NSF.lanNode = 0; // LanNodeの数
-
-  // nsf-main var
-  NSF.mainDropFlg     = true; // Draggableのフラグ
-  NSF.dropNodeInt     = 0;    // DropNodeの個数
-  NSF.dropNodeName    = "";   // DropNodeの名前
-  NSF.dropContextName = "";   // DropNodeのContextMenuの名前
-
-  // nsf-canvas var
-  NSF.points          = []; // ドラッグ時のマウスの座標の配列
-  NSF.lanArrClass     = []; // nsf-main-canvasのクラスの配列
-  NSF.lanArrWidth     = []; // LANの帯域幅の配列
-  NSF.lanWidth        = 2;  // LANの幅
-  NSF.lanFlag         = false;  // LANのフラグ   (画像に乗っているとき)
-  NSF.busFlag        = false;
-  NSF.busDrawFrag    = false;
-  NSF.lanFlaglink;          // LANのフラグ2  (lanLinkがあるとき)
-  NSF.lanFlagPoint;         // LANのフラグ3  (lanDownが実行されたとき)
-  NSF.lanFlagDelet;         // LANのフラグ4  (lanDownの削除するとき)
-  NSF.lanFlagMove;          // LANのフラグ5  (lanMoveDownが実行されたとき)
-  NSF.elLanMoveThis;        // LANMoveのクエリのキャッシュ
-  NSF.addCtx;               // 追加したCanvasのAPIにアクセスできるオブジェクト
-  NSF.addCanvas;            // 追加したCanvasを格納
-  NSF.canvasWidth      = $('#nsf-main').width();  // nsf-mainの幅
-  NSF.canvasHeight     = $('#nsf-main').height(); // nsf-mainの高さ
-  NSF.mainCanvasWidth  = $('#nsf-main-canvas')[0].getBoundingClientRect().left - 35;
-  NSF.mainCanvasHeight = $('#nsf-main-canvas')[0].getBoundingClientRect().top - 35;
-  NSF.mainCanvasX      = $('#nsf-main-canvas')[0].getBoundingClientRect().left;
-  NSF.mainCanvasY      = $('#nsf-main-canvas')[0].getBoundingClientRect().top;
-  NSF.questionCanvasX  = $('#nsf-main-canvas')[0].getBoundingClientRect().left + 150;
-  NSF.questionCanvasY  = $('#nsf-main-canvas')[0].getBoundingClientRect().top + 60;
-  NSF.mainCtx          = $('#nsf-main-canvas')[0].getContext('2d'); // nsf-main-canvasをAPIにアクセスできるオブジェクト
-  NSF.mainCtx.strokeStyle = '#2fb9fe';                              // 線の色を青色に設定
-
-  // nsf-glay var
-  NSF.bGlayFladg = false; // glayLayerのフラグ
-
-  //animecanvasのフラグ
-  NSF.animecanvasFlag = false;
-
-  //urlパラメータを取得
-  var arg = new Object;
-  var pair=location.search.substring(1).split('&');
-  for(var i=0;pair[i];i++) {
-      var kv = pair[i].split('=');
-      NSF.urlparameter=kv[1];
-  }
-
-  //nsf-mainのxy
-  $('#nsf-main-canvas').attr('data-x', NSF.mainCanvasWidth);
-  $('#nsf-main-canvas').attr('data-y', NSF.mainCanvasHeight);
 
 
 
