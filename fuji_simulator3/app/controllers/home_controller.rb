f6a256660c0c18f1eb60910445455c18c1293d1f
菅原良太 <>
2019-01-18 22:37:03 +0900

    予稿８割完成　やっぱり沼田のところ治ってないと思うんだけど、わからないから下手にいじれない

--- fuji_simulator3/app/controllers/home_controller.rb
+++ fuji_simulator3/app/controllers/home_controller.rb
@@ -3,15 +3,19 @@ class HomeController < ApplicationController
   require 'json'
 
   def top
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
   end
 
   def main
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
   end
 
   def temp
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
   end
 
   def create
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
   end
 
   def net_check
